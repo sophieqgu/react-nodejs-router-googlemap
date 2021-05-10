86d91123dce94e3c95d5f593e8032aab4726624d
Sophie Gu <sophieqgu@gmail.com>
2021-05-10 09:32:58 -0400

    Create project and TopBar component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import logo from '../assets/images/logo.svg';
+import TopBar from './TopBar';
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+    </div>
+  );
+}
+
+export default App;
