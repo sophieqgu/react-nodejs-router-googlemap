ba3a73e53b0ae761d964908f8fdaae5cfeab31b4
Sophie Gu <sophieqgu@gmail.com>
2021-05-16 19:32:47 -0400

    Update app component to handle persistent login

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
+import React, {Component} from 'react';
 import TopBar from './TopBar';
 import Main from './Main'
 
-function App() {
+import { TOKEN_KEY } from '../constants';
+
+class App extends Component {
+  state = {
+     isLoggedIn: Boolean(localStorage.getItem(TOKEN_KEY)),
+  }
+
+  handleLoginSucceed = (token) => {
+       console.log('token --- ', token)
+       localStorage.setItem(TOKEN_KEY, token)
+       this.setState({ isLoggedIn: true });
+  }
+
   return (
     <div className="App">
       <TopBar />
-      <Main />
+      <Main
+          handleLoginSucceed={this.handleLoginSucceed}
+          isLoggedIn={this.state.isLoggedIn}
+      />
     </div>
   );
 }
