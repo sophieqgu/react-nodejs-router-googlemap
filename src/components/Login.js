85f0b0487d4909a4c7e0639f2697044d9155a0da
Sophie Gu <sophieqgu@gmail.com>
2021-05-16 20:11:11 -0400

    Check if user is logged in in main

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Form, Icon, Input, Button, message } from 'antd';
 import { Link } from 'react-router-dom';
 import { API_ROOT } from '../constants';
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,23 @@
-import React, {Component} from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { Register } from './Register'
 import Login from './Login'
 
 class Main extends Component {
-   render() {
+    getLogin = () => {
+       return this.props.isLoggedIn ? <Redirect to="/home"/> : <Login handleLoginSucceed={this.props.handleLoginSucceed}/>;
+    }
+
+    render() {
        return (
            <div className="main">
               <Switch>
-                <Route path="/login" component={Login}/>
+                <Route path="/login" render={this.getLogin}/>
                 <Route path="/register" component={Register}/>
               </Switch>
            </div>
        );
-   }
+    }
 }
 
 export default Main;
