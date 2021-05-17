359cfd0bcb2c1116f70820dd21260c6e85735a02
Sophie Gu <sophieqgu@gmail.com>
2021-05-16 23:53:42 -0400

    Save geolocation in localStorage

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,19 @@ import { GEO_OPTIONS, POS_KEY, API_ROOT, AUTH_HEADER, TOKEN_KEY } from '../const
 const { TabPane } = Tabs;
 
 class Home extends Component {
+    state = {
+      isLoadingGeoLocation: false,
+      error: ''
+    }
     componentDidMount() {
       console.log(navigator.geolocation);
       if ("geolocation" in navigator) {
-
-         navigator.geolocation.getCurrentPosition(
-             this.onSuccessLoadGeoLocation,
-             this.onFailedLoadGeoLocation,
-             GEO_OPTIONS,
-         );
+        this.setState({ isLoadingGeoLocation: true, error: ''});
+        navigator.geolocation.getCurrentPosition(
+           this.onSuccessLoadGeoLocation,
+           this.onFailedLoadGeoLocation,
+           GEO_OPTIONS,
+        );
       } else {
          this.setState({ error: 'Geolocation is not supported.'});
       }
@@ -21,12 +25,14 @@ class Home extends Component {
 
     onSuccessLoadGeoLocation = (position) => {
       console.log(position);
-
+      const { latitude, longitude } = position.coords;
+      localStorage.setItem(POS_KEY, JSON.stringify({ lat: latitude, lon: longitude }));
+      this.setState({ isLoadingGeoLocation: false, error: '' });
     }
 
 
     onFailedLoadGeoLocation = () => {
-      console.log(position);
+      this.setState({ isLoadingGeoLocation: false, error: 'Failed to load geo location.' });
     }
 
 
