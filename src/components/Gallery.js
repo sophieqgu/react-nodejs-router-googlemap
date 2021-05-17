8e7b8937563612cbb74d34ef0e8118d8c3125e1d
Sophie Gu <sophieqgu@gmail.com>
2021-05-17 08:56:28 -0400

    Add typechecking with PropTypes

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
 import GridGallery from 'react-grid-gallery';
+import PropTypes from 'prop-types';
 
 class Gallery extends Component {
-   render() {
-       return (
-           <div>
-               Gallery
-           </div>
-       );
-   }
+
+  static propTypes = {
+     images: PropTypes.arrayOf(
+         PropTypes.shape({
+             user: PropTypes.string.isRequired,
+             src: PropTypes.string.isRequired,
+             thumbnail: PropTypes.string.isRequired,
+             caption: PropTypes.string,
+             thumbnailWidth: PropTypes.number.isRequired,
+             thumbnailHeight: PropTypes.number.isRequired
+         })
+     ).isRequired
+  }
+
+  render() {
+     return (
+         <div>
+             Gallery
+         </div>
+     );
+  }
 }
 export default Gallery;
